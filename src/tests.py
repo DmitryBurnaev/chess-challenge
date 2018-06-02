81784c52a7bc807d498f65c8a84299fa69209581
Dmitry <dmitry.burnaev@gmail.com1~>
2018-06-02 16:14:41 +0300

    Test coverage for creating various combinations

--- src/tests.py
+++ src/tests.py
@@ -1,5 +1,5 @@
 from src.exceptions import CanNotCreateGameInstance
-from src.figures import Queen, King, Rook, Bishop, Knight
+from src.figures import Queen, King, Rook
 from src.game_logic import Board, Game
 
 import unittest
@@ -25,20 +25,12 @@ class GameInitialTestCase(unittest.TestCase):
         self.assertEqual(game.possible_figures.count(Queen), q_count)
 
     def test_fail_for_fake_combinations(self):
-        try:
+        with self.assertRaises(CanNotCreateGameInstance):
             Game(2, 1, {'kings': 3, 'rooks': 2})
-        except CanNotCreateGameInstance as e:
-            self.assertIsNotNone(e)
-        else:
-            raise AssertionError('Created game for fake combinations')
 
     def test_fail_for_null_size_boards(self):
-        try:
+        with self.assertRaises(CanNotCreateGameInstance):
             Game(0, 0, {})
-        except CanNotCreateGameInstance:
-            pass
-        else:
-            raise AssertionError('Created game null-size board')
 
 
 class FigureAttackTestCase(unittest.TestCase):
@@ -125,16 +117,167 @@ class FigureAttackTestCase(unittest.TestCase):
 
 class FillBoardTestCase(unittest.TestCase):
 
-    def test_cells_to_attack(self):
-        self.assertIsNone(None)
-        pass
+    def test_simple_board(self):
+        # ---------Initial configuration----------
+        # Boards dimensions: 3 x 2
+        # Figures set:
+        #   Kings   :  1
+        #   Rooks   :  1
+        # -----------------Result-----------------
+        # Found 4 combinations:
+        # [R] Rook (1;2) | [K] King (3;1)
+        #     1 2 3
+        # 1 | - - K
+        # 2 | R - -
+        # --------------------
+        # [R] Rook (3;2) | [K] King (1;1)
+        #     1 2 3
+        # 1 | K - -
+        # 2 | - - R
+        # --------------------
+        # [R] Rook (1;1) | [K] King (3;2)
+        #     1 2 3
+        # 1 | R - -
+        # 2 | - - K
+        # --------------------
+        # [R] Rook (3;1) | [K] King (1;2)
+        #     1 2 3
+        # 1 | - - R
+        # 2 | K - -
+        # --------------------
+        combination_1 = [
+            {'type': 'Rook', 'pos_x': 0, 'pos_y': 1, 'display_char': 'R'},
+            {'type': 'King', 'pos_x': 2, 'pos_y': 0, 'display_char': 'K'},
+        ]
+        combination_2 = [
+            {'type': 'Rook', 'pos_x': 2, 'pos_y': 1, 'display_char': 'R'},
+            {'type': 'King', 'pos_x': 0, 'pos_y': 0, 'display_char': 'K'},
+        ]
+        combination_3 = [
+            {'type': 'Rook', 'pos_x': 0, 'pos_y': 0, 'display_char': 'R'},
+            {'type': 'King', 'pos_x': 2, 'pos_y': 1, 'display_char': 'K'},
+        ]
+        combination_4 = [
+            {'type': 'Rook', 'pos_x': 2, 'pos_y': 1, 'display_char': 'R'},
+            {'type': 'King', 'pos_x': 0, 'pos_y': 0, 'display_char': 'K'},
+        ]
 
-    def test_fill_board(self):
-        pass
+        game = Game(3, 2, {'kings': 1, 'rooks': 1})
+        game.generate_combinations()
+        result_boards = game.serialized_boards.values()
+        self.assertEqual(len(result_boards), 4)
+        self.assertIn(combination_1, result_boards)
+        self.assertIn(combination_2, result_boards)
+        self.assertIn(combination_3, result_boards)
+        self.assertIn(combination_4, result_boards)
+
+    def test_all_figures_combinations(self):
+        # ---------Initial configuration----------
+        # Boards dimensions: 4 x 4
+        # Figures set:
+        #  Rooks    :  1
+        #  Knights  :  1
+        #  Bishops  :  1
+        #  Queens   :  1
+        #  Kings    :  1
+        # -----------------Result-----------------
+        # Found 16 combinations:
+        # [Q] Queen (3;4) | [B] Bishop (2;1) | [R] Rook (4;2) | [K] King (1;3)
+        # | [N] Knight (1;1)
+        #     1 2 3 4
+        # 1 | N B - -
+        # 2 | - - - R
+        # 3 | K - - -
+        # 4 | - - Q -
+        # --------------------
+        # [Q] Queen (2;1) | [B] Bishop (3;4) | [R] Rook (1;3) | [K] King (4;2)
+        # | [N] Knight (4;4)
+        #     1 2 3 4
+        # 1 | - Q - -
+        # 2 | - - - K
+        # 3 | R - - -
+        # 4 | - - B N
+
+        # --------------------
+        # [Q] Queen (3;1) | [B] Bishop (2;4) | [R] Rook (4;3) | [K] King (1;2)
+        # | [N] Knight (1;4)
+        #     1 2 3 4
+        # 1 | - - Q -
+        # 2 | K - - -
+        # 3 | - - - R
+        # 4 | N B - -
+        # --------------------
+        # [Q] Queen (2;4) | [B] Bishop (1;2) | [R] Rook (4;3) | [K] King (3;1)
+        # | [N] Knight (1;1)
+        #     1 2 3 4
+        # 1 | N - K -
+        # 2 | B - - -
+        # 3 | - - - R
+        # 4 | - Q - -
+        # --------------------
+
+        combination_1 = [
+            {'type': 'Queen', 'pos_x': 2, 'pos_y': 3, 'display_char': 'Q'},
+            {'type': 'Bishop', 'pos_x': 1, 'pos_y': 0, 'display_char': 'B'},
+            {'type': 'Rook', 'pos_x': 3, 'pos_y': 1, 'display_char': 'R'},
+            {'type': 'King', 'pos_x': 0, 'pos_y': 2, 'display_char': 'K'},
+            {'type': 'Knight', 'pos_x': 0, 'pos_y': 0, 'display_char': 'N'},
+        ]
+        combination_2 = [
+            {'type': 'Queen', 'pos_x': 1, 'pos_y': 0, 'display_char': 'Q'},
+            {'type': 'Bishop', 'pos_x': 2, 'pos_y': 3, 'display_char': 'B'},
+            {'type': 'Rook', 'pos_x': 0, 'pos_y': 2, 'display_char': 'R'},
+            {'type': 'King', 'pos_x': 3, 'pos_y': 1, 'display_char': 'K'},
+            {'type': 'Knight', 'pos_x': 3, 'pos_y': 3, 'display_char': 'N'},
+        ]
+        combination_3 = [
+            {'type': 'Queen', 'pos_x': 2, 'pos_y': 0, 'display_char': 'Q'},
+            {'type': 'Bishop', 'pos_x': 1, 'pos_y': 3, 'display_char': 'B'},
+            {'type': 'Rook', 'pos_x': 3, 'pos_y': 2, 'display_char': 'R'},
+            {'type': 'King', 'pos_x': 0, 'pos_y': 1, 'display_char': 'K'},
+            {'type': 'Knight', 'pos_x': 0, 'pos_y': 3, 'display_char': 'N'},
+        ]
+        combination_4 = [
+            {'type': 'Queen', 'pos_x': 1, 'pos_y': 3, 'display_char': 'Q'},
+            {'type': 'Bishop', 'pos_x': 0, 'pos_y': 1, 'display_char': 'B'},
+            {'type': 'Rook', 'pos_x': 3, 'pos_y': 2, 'display_char': 'R'},
+            {'type': 'King', 'pos_x': 2, 'pos_y': 0, 'display_char': 'K'},
+            {'type': 'Knight', 'pos_x': 0, 'pos_y': 0, 'display_char': 'N'},
+        ]
+
+        game = Game(4, 4, {'kings': 1, 'rooks': 1, 'knights': 1, 'queens': 1,
+                           'bishops': 1})
+        game.generate_combinations()
+        result_boards = game.serialized_boards.values()
+        self.assertEqual(len(result_boards), 16)
+        self.assertIn(combination_1, result_boards)
+        self.assertIn(combination_2, result_boards)
+        self.assertIn(combination_3, result_boards)
+        self.assertIn(combination_4, result_boards)
+
+
+# @contextmanager
+# def capture(command, *args, **kwargs):
+#     out, sys.stdout = sys.stdout, StringIO()
+#     try:
+#         command(*args, **kwargs)
+#         sys.stdout.seek(0)
+#         yield sys.stdout.read()
+#     finally:
+#         sys.stdout = out
+#
+#
+# class RunModeTestCase(unittest.TestCase):
+#     def test_render_initial_data(self):
+#         game = Game(4, 3, {'kings': 3, 'rooks': 2})
+#         with capture(game.render_initial_data) as output:
+#             msg = 'Boards dimensions: {} x {}'.format(
+#                 game.dimension_x, game.dimension_y
+#             )
+#             self.assertIn(msg, output)
+#             self.assertIn('Kings:3', output.replace(' ', ''))
+#             self.assertIn('Rooks:2', output.replace(' ', ''))
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
